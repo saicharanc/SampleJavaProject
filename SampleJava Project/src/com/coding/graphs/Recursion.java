20e47bc2f0513a8209d209b6881c4b7c09807577
saicharanc <saicharanc@gmail.com>
2018-08-11 19:13:58 -0500

    Graphs and recursion

---
+++ SampleJava Project/src/com/coding/graphs/Recursion.java
@@ -0,0 +1,34 @@
+package com.coding.graphs;
+
+import java.io.File;
+
+/**
+ * A function that calls itself is a recursive function.
+ * @author SaiCharanChalasani
+ * Example of a recursive function to find the number of txt files in a computer.
+ */
+
+/*public class Recursion {
+
+	public int countTxt(Directory dir){
+		
+		if(dir == null){
+			return 0;
+		}
+		
+		int count;
+		
+		for(File file : dir){
+			if(file.endsWith(".txt")){
+				count ++;
+			}
+		}
+		
+		for (Directory subDir : dir){
+			count + = countTxt(subDir);
+		}
+	} 
+	 return count;
+	
+}
+*/
